e4a370f0c8c0e8813e08856429326e457f535b4c
unknown <piniek.adrian@gmail.com>
2022-06-10 16:02:41 +0200

    Some stupid ideas

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include "main.hpp"
 
+enum Tag
+{
+    REQ,
+    ACK,
+    SREQ // Student request for wine
+};
 
-enum Tag {
-        REQ, 
-        ACK,
-        SREQ //Student request for wine
-    };
-
-struct Msg {
+struct Msg
+{
     Tag tag;
     int clock;
     int studentRank;
 };
-
 
 int const WINE_MAKERS = 1;
 int const STUDENTS = 1;
@@ -24,83 +24,89 @@ int const MIN_ACK = WINE_MAKERS - SAFE_PLACES;
 int const MAX_WINE = 10;
 int const MIN_WINE = 1;
 
-int const MIN_TIME_WAIT = 500; // ms 
-int const MAX_TIME_WAIT = 1500; //ms
+int const MIN_TIME_WAIT = 500;  // ms
+int const MAX_TIME_WAIT = 1500; // ms
 
 int myClock = 0;
 int myRank;
 int maxRank = WINE_MAKERS + STUDENTS;
 
+int Studenciaki[STUDENTS] = {0};
+
 MPI_Request request = {0};
 MPI_Status status = {0};
 
-
-void debug(Msg &msg, char* info){
+void debug(Msg &msg, char *info)
+{
     printf("[%d] %s tag:%d \n", myRank, info, msg.tag);
 }
 
-
-void tick(int newClock = 0){
+void tick(int newClock = 0)
+{
     myClock = std::max(newClock, myClock) + 1;
 }
 
-void sendMsg(int destination, Tag tag, int studentRank=0){
+void sendMsg(int destination, Tag tag, int studentRank = 0)
+{
 
-    Msg msg {
+    Msg msg{
         .tag = tag,
         .clock = myClock,
-        .studentRank = studentRank
-    };
-    char * info = "Send";
+        .studentRank = studentRank};
+    char *info = "Send";
     sprintf(info, "%d", destination);
     debug(msg, info);
     MPI_Send(&msg, sizeof(Msg), MPI_BYTE, destination, 0, MPI_COMM_WORLD);
 }
 
-bool recvMsgWait(int *source, Msg *msg, int mpiTag = MPI_ANY_TAG){
+bool recvMsgWait(int *source, Msg *msg, int mpiTag = MPI_ANY_TAG)
+{
     MPI_Recv(msg,
-        sizeof(Msg),
-        MPI_BYTE,
-        MPI_ANY_SOURCE,
-        mpiTag,
-        MPI_COMM_WORLD,
-        &status
-    );
+             sizeof(Msg),
+             MPI_BYTE,
+             MPI_ANY_SOURCE,
+             mpiTag,
+             MPI_COMM_WORLD,
+             &status);
     debug(*msg, "Recv");
     int completed;
-	MPI_Test(&request, &completed, &status);
-    if(!completed)
+    MPI_Test(&request, &completed, &status);
+    if (!completed)
         return false;
     else
         return true;
 }
 
-bool recvMsgNoWait(int *source, Msg *msg){
+bool recvMsgNoWait(int *source, Msg *msg)
+{
     MPI_Irecv(msg,
-        sizeof(Msg),
-        MPI_BYTE,
-        MPI_ANY_SOURCE,
-        MPI_ANY_TAG,
-        MPI_COMM_WORLD,
-        &request
-    );
+              sizeof(Msg),
+              MPI_BYTE,
+              MPI_ANY_SOURCE,
+              MPI_ANY_TAG,
+              MPI_COMM_WORLD,
+              &request);
     debug(*msg, "Recv");
     int completed;
-	MPI_Test(&request, &completed, &status);
-    if(!completed)
+    MPI_Test(&request, &completed, &status);
+    if (!completed)
         return false;
     else
         return true;
 }
 
-void consumeClock(){
+void consumeClock()
+{
     usleep(rand() % (MAX_TIME_WAIT - MIN_TIME_WAIT) + MIN_TIME_WAIT);
 }
 
-void students(){
-    while(true){
+void students()
+{
+    while (true)
+    {
         consumeClock();
-        for(int i = 0; i < WINE_MAKERS; i++){
+        for (int i = 0; i < WINE_MAKERS; i++)
+        {
             sendMsg(i, REQ);
         }
         int source;
@@ -110,55 +116,65 @@ void students(){
     }
 }
 
-
-void sendFackOffToTheRestOFWineMakers(){
-    for(int i = 0; i < WINE_MAKERS; i++){
-        if(i != myRank)
+void sendFackOffToTheRestOFWineMakers()
+{
+    for (int i = 0; i < WINE_MAKERS; i++)
+    {
+        if (i != myRank)
             sendMsg(i, ACK);
     }
 }
 
-
 // @TODO
-int getStudent(){
+int getStudent()
+{
     int studentRank = 0;
     Msg msg;
 
-    while(!studentRank)
-        recvMsgWait(&studentRank,&msg, SREQ);
-        
-    return studentRank; 
+    while (!studentRank)
+        recvMsgWait(&studentRank, &msg, SREQ);
+
+    return studentRank;
 }
 
-void wineMakers(){
-    while(true){
+void wineMakers()
+{
+    while (true)
+    {
         // produkujemy wino
         consumeClock();
         int myStudentRank = getStudent();
-        //wyslij ze chcesz sie wymieniac
-        for(int i = 0; i < WINE_MAKERS; i++){
-            if(i != myRank)
+        // wyslij ze chcesz sie wymieniac
+        for (int i = 0; i < WINE_MAKERS; i++)
+        {
+            if (i != myRank)
+                // IDEA: Wyślij w trakcie requesta id studenta
                 sendMsg(i, REQ);
         }
-        
+
         // czekaj na akceptacje od innych winiarzy
         int ackCounter = 0;
-        while(ackCounter < MIN_ACK){
+        while (ackCounter < MIN_ACK)
+        {
             int source;
             Msg msg;
             recvMsgWait(&source, &msg);
-            if(msg.tag = ACK){
+            if (msg.tag = ACK)
+            {
                 ackCounter++;
                 continue;
             }
-            
-            if(msg.clock < myClock){
+
+            if (msg.clock < myClock)
+            {
                 sendMsg(source, ACK);
-                if(msg.studentRank == myStudentRank){
+                if (msg.studentRank == myStudentRank)
+                {
                     myStudentRank = getStudent();
                 }
             }
-            else{ //msg.clock >= myClock
+            else
+            { // msg.clock >= myClock
                 ackCounter++;
             }
             tick(msg.clock);
@@ -184,20 +200,29 @@ void wineMakers(){
     }
 }
 
-
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	MPI_Comm_size(MPI_COMM_WORLD, &maxRank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &maxRank);
 
     printf("INIT [%d] \n", myRank);
 
-    if(myRank < WINE_MAKERS)
+    if (myRank < WINE_MAKERS)
         wineMakers();
     else
-        students();    
+        students();
 
-	MPI_Finalize();
+    MPI_Finalize();
     return 0;
 }
+
+// MOTYW DLA WYBRANIA STUDENTA
+/*
+W CZASIE prośby o wolne miejsca, daje id studenta któremu odpalam wino,
+przy sprawdzaniu ACK sprawdzam swojego studenta z wiadomością. Jeżeli są identyczni i mam większego lamporta,
+muszę wybrać nowego studenta. Jednocześnie zapisuje id winiarza aby znowu mu nie wysłać ACK.(??????????)
+Dodatkowo wybierając nowego studenta od nowa startuje z wysyłaniem ACK (?????? Podchodzi pod niezły młyn w wiadomościach)
+Winiarz śpi z studentem, wolny student staje się po sleepie u Winiarza, winiarz wtedy wybiera nowego losowego studenta.
+
+*/
