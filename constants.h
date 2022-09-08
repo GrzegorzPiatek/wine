4df5d98dde5e8c8ac76f1b84fb171364d3883ebe
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2022-09-08 17:32:47 +0200

    resolfe compiler problems with constants.h

--- constants.h
+++ constants.h
@@ -1,8 +1,24 @@
-#pragma ones
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
 #define REQ 0
 #define ACK 1
 #define UPD 2 // tell students to update offer after exchange
+
+#define WINE_MAKERS 2
+#define STUDENTS 2
+
+#define SAFE_PLACES 1
+
+#define MIN_ACK WINE_MAKERS - SAFE_PLACES
+
+#define MIN_TIME_WAIT 1
+#define MAX_TIME_WAIT 2
+
+#define MAX_RANK  WINE_MAKERS + STUDENTS
+
+#define MAX_WINE 10
+
 
 struct Msg {
     int clock;
@@ -10,18 +26,4 @@ struct Msg {
     int wine;
 };
 
-int isDebugOn = 0;
-
-#define WINE_MAKERS 2;
-#define STUDENTS 2;
-
-#define SAFE_PLACES 1;
-
-#define MIN_ACK WINE_MAKERS - SAFE_PLACES;
-
-#define MIN_TIME_WAIT 1; 
-#define MAX_TIME_WAIT 2; 
-
-#define MAX_RANK  WINE_MAKERS + STUDENTS;
-
-#define MAX_WINE 10
+#endif //CONSTANTS_H
